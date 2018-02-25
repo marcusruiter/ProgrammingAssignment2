e5a1fcac28ad16af6cf18abb6a76a5182c261914
marcusruiter <36812055+marcusruiter@users.noreply.github.com>
2018-02-25 21:58:32 +0100

    For submit makeCacheMatrix() and cacheSolve()

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,9 @@
-## functions do
-
 ## ******************* makeCacheMatrix() ******************************
 ## The function makeCacheMatrix caches the original Matrix passed as argument in the global environment using <<-
 ## The methods of the function set, get allow for the setting and getting of the original matrix
 ## The methods of the function setinv, getinv allow for the setting and getting of the inverse of the original matrix
 ## The return object is a list of the methods set, get, setinv, getinv
 
-## Write a short comment describing this function
 makeCacheMatrix <- function(cacheMatrix = matrix()) {
         
 	cacheMatrixinv <- NULL
